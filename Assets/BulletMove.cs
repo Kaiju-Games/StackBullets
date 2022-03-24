43d819aa4c688840515f084a51e177dab3930fd3
denoboi <deniz.kaymakci@hotmail.com>
2022-03-24 14:30:40 +0300

    Spline Width +

--- Assets/BulletMove.cs
+++ Assets/BulletMove.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class BulletMove : MonoBehaviour
 {
-   
+    private float _speed = 5f;
+
+    Rigidbody _bulletRb;
+
+
+    private void Start()
+    {
+        _bulletRb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        _bulletRb.AddForce(Vector3.forward * _speed, ForceMode.Impulse);
+    }
 }
